178c11ddd597fb60b66bcdba4e1cb65664c84329
Raushan Gupta <raushangupta1425@gmail.com>
2024-05-14 18:10:59 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,48 +1,10 @@
-let btn1=document.querySelector("#btn1");
-btn1.addEventListener("click",()=>{
-    btn1.innerText="Web Awareness: Recognizing the capabilities of the modern internet, which is interactive and user-generated, as opposed to the staticweb pages of the past.";
-});
-let btn2=document.querySelector("#btn2");
-btn2.addEventListener("click",()=>{
-    btn2.innerText="Digital Literacy and Content Creation: Acquiring the skills to create various types of digital content such as videos, images, text, and graphics. This also includes understanding how to use digital tools and platforms effectively.";
-});
-let btn3=document.querySelector("#btn3");
-btn3.addEventListener("click",()=>{
-    btn3.innerText="Mature Social Networking: Using social media platforms responsibly and ethically, understanding privacy settings, and managing digital footprints to ensure a positive and safe online presence.";
-});
-let btn4=document.querySelector("#btn4");
-btn4.addEventListener("click",()=>{
-    btn4.innerText="Professional Networking: Leveraging professional platforms like LinkedIn to build and maintain a career-oriented network, sharing appropriate content, and engaging in professional growth activities.";
-});
-let btn5=document.querySelector("#btn5");
-btn5.addEventListener("click",()=>{
-    btn5.innerText="Effective Forum Participation: Engaging in online forums and discussion boards in a constructive manner, respecting others' opinions, and valuable insights.";
-});
-let btn6=document.querySelector("#btn6");
-btn6.addEventListener("click",()=>{
-    btn6.innerText="Blogging and Video Blogging: Creating meaningful and respectful blog content that adds value to discussions, respects copyright laws, and adheres to online etiquette.";
-});
-let btn7=document.querySelector("#btn7");
-btn7.addEventListener("click",()=>{
-    btn7.innerText="Search Literacy: Developing strategies to search for information effectively online, discerning reliable sources from unreliable ones, and using advanced search techniques to refine results.";
-});
-let btn8=document.querySelector("#btn8");
-btn8.addEventListener("click",()=>{
-    btn8.innerText="Using Open Courseware and MOOCs: Utilizing massive open online courses and open educational resources for self-learning and professional development, understanding how to choose courses that enhance personal and professional skills.";
-});
-let btn9=document.querySelector("#btn9");
-btn9.addEventListener("click",()=>{
-    btn9.innerText="Online Safety: Protecting oneself from online threats such as malware, phishing, cyberbullying, and scams. This includes using security tools like antivirus software and understanding the importance of secure browsing .";
-});
-let btn10=document.querySelector("#btn10");
-btn10.addEventListener("click",()=>{
-    btn10.innerText="Reputation Management: Building and maintaining a positive online reputation,understanding the long-term impacts of online actions, and managing personal data to prevent misuse.";
-});
-let btn11=document.querySelector("#btn11");
-btn11.addEventListener("click",()=>{
-    btn11.innerText="Digital Ethics: Understanding the ethical implications of online behavior,respecting intellectual property, avoiding plagiarism, and engaging in interactions that are honest and respectful.";
-});
-let btn12=document.querySelector("#btn12");
-btn12.addEventListener("click",()=>{
-    btn12.innerText="Netiquette: Adhering to internet etiquette, understanding the correct behaviors and communications in various online environments, and avoiding aggressive or inappropriate behavior.";
-});
+// let items = document.getElementsByClassName("items");
+// let itemBox = document.getElementsByClassName("item");
+
+
+// for(i=0;i<items.length;i++){
+//     items[i].addEventListener('click', ()=>{
+//         itemBox[i].className = "bg-light";
+//     })
+// }
+
